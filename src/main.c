7bccc63270785b803282248823ba4481a2196e0e
Gustavo <gustavoms7@al.insper.edu.br>
2023-11-17 01:27:27 -0300

    cpp

--- src/main.c
+++ src/main.c
@@ -325,14 +325,14 @@ void configure_lvgl(void) {
 	disp_drv.hor_res = LV_HOR_RES_MAX;      /*Set the horizontal resolution in pixels*/
 	disp_drv.ver_res = LV_VER_RES_MAX;      /*Set the vertical resolution in pixels*/
 
-	lv_disp_t * disp;
+	
 	lv_disp_drv_register(&disp_drv); /*Register the driver and save the created display objects*/
 	
 	/* Init input on LVGL */
 	lv_indev_drv_init(&indev_drv);
 	indev_drv.type = LV_INDEV_TYPE_POINTER;
 	indev_drv.read_cb = my_input_read;
-	lv_indev_t * my_indev = lv_indev_drv_register(&indev_drv);
+	lv_indev_drv_register(&indev_drv);
 }
 
 /************************************************************************/
